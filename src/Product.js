222d6841b98dc93458b65e3f147cb740938be942
Gabriel Guerreiro <gblguerreiro@gmail.com>
2021-01-19 22:12:03 +0100

    Counter added

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import "./Product.css";
 import StarRateIcon from '@material-ui/icons/StarRate';
+import { useStateValue } from "./StateProvider";
 
-function Product( {title, image, price, rating}) {
+function Product( {id, title, image, price, rating}) {
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+
+        });
+    };
+    
     return (
         <div className='product'>
             <div className="product__info">
@@ -24,7 +42,7 @@ function Product( {title, image, price, rating}) {
                 src={image}
                 alt=""   
             />
-            <button> Add to Basket </button>    
+            <button onClick={addToBasket}> Add to Basket </button>  
         </div>
     )
 }
