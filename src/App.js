76ff89898e18d539259953000dd12526bdd16997
Gabriel Guerreiro <gblguerreiro@gmail.com>
2021-01-19 14:10:51 +0100

    added Router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 function App() {
   return (
     //BEM
-    <div className="app">
-        
-        <Header />
-
-        <Home />
-    </div>
+    <Router>
+      <div className="app">
+          <Switch>
+            <Route path="/checkout">
+              <Header />
+            </Route>          
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>  
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
