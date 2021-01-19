71f2bae96bf241a1d2b97acbd47651fb73c58619
Gabriel Guerreiro <gblguerreiro@gmail.com>
2021-01-19 16:30:58 +0100

    Added checkout, second part

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
-            <img
-                className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />    
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />  
+            
+            </Link>
+              
             <div className="header__search">
                 <input 
                     className="header__searchInput"
@@ -47,11 +52,14 @@ function Header() {
                     <span 
                         className='header__optionLineTwo'> Prime
                     </span>
-                </div>  
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount"> 0 </span>
-                </div>                    
+                </div> 
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount"> 0 </span>
+                    </div>                 
+                </Link> 
+                                 
             </div> 
                   
         </div>
