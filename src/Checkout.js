5e7429cf56e2aabf22761bce47a7fcd21debacb0
Gabriel Guerreiro <gblguerreiro@gmail.com>
2021-01-19 17:02:50 +0100

    New changes on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
     return (
@@ -23,7 +24,7 @@ function Checkout() {
             </div>
             <div className="checkout__right">
                 <h2>
-                    The subtotal will go here
+                    <Subtotal />
                 </h2>
             </div>
         
