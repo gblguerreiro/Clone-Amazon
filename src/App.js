38cfa800f9506911058a33912e342062e879498e
Gabriel Guerreiro <gblguerreiro@gmail.com>
2021-01-20 02:17:23 +0100

    Login page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
-  return (
+  return (  
     //BEM
     <Router>
       <div className="app">
-      <Header />
           <Switch>
-            <Route path="/checkout">              
+            <Route path="/login">
+              <Login />
+             </Route> 
+            <Route path="/checkout">  
+              <Header />            
               <Checkout />
             </Route>          
             <Route path="/">
+              <Header />
               <Home />
             </Route>  
           </Switch>
